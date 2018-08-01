6dc2836a1799e186295a1b9a379f83a1618a7861
Jack "Rocko" Bauman <jcbauman@Ucsc.edu>
2018-08-01 16:45:07 -0700

    search now works (slow)

--- SearchTableViewController.swift
+++ SearchTableViewController.swift
@@ -25,35 +25,37 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
     var posts = [post]()
     typealias JSONStandard = [String: AnyObject]
     
-    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    //when search bar triggered, begin search
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let keywords = searchBar.text
-        print("hiding keyboard000909090909")
         self.view.endEditing(true)
         let finalKeywords = keywords?.replacingOccurrences(of: " ", with: "%20")
-        searchURL = "https://api.spotify.com/v1/search?q=\(finalKeywords!)&type=track&market=US&limit=20"
+        searchURL = "https://api.spotify.com/v1/search?q=\(finalKeywords!)&type=track&market=US&limit=10"
         getAlamoAuth()
-
     }
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     //get an authorization token for search and input link
     func getAlamoAuth(){
-        let parameters = ["client_id" : "a5656d6d551e647cb98f7c561ecb245b5",
-                          "client_secret" : "3bf2f4cf94e440908654d356ab2fe3cf",
+        let parameters = ["client_id" : "a5656d6551e647cb98f7c561ecb245b5",
+                          "client_secret" : "9513db168f93452c82f60b1e26280877",
                           "grant_type" : "client_credentials"]
         Alamofire.request("https://accounts.spotify.com/api/token", method: .post, parameters: parameters).responseJSON(completionHandler: {
             response in
             switch response.result{
                 case .success(let value):
                 let json = JSON(value)
+                print(json)
                 self.accessToken = json["access_token"].stringValue
                 
                 //API call after token obtained
+                print("gotAuth!")
+                print(self.accessToken)
                 self.callAlamo(url: self.searchURL)
             case .failure(let error):
                 print(error)
@@ -72,6 +74,7 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
         Alamofire.request(url, headers: headers).responseJSON(completionHandler:{
             response in
             self.parseData(JSONData: response.data!)
+            print(response)
         })
     }
 
@@ -104,6 +107,7 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
         }catch{
             print(error)
         }
+        print("done")
     }
     
     override func didReceiveMemoryWarning() {
